6cae7c84f14c98fa5969f999b5931b2b18b33447
bengetch <bengetch@gmail.com>
2024-06-06 07:38:12 -0400

    Restrict NadaArray and utility functions to SecretInteger inputs

--- src/nada_data/basic_ops.py
+++ src/nada_data/basic_ops.py
@@ -1,83 +1,72 @@
 from typing import List, Union
 from nada_dsl import (
-    SecretInteger, PublicInteger, Integer, audit
+    SecretInteger, audit
 )
 from nada_data.nada_array import NadaArray
 
 
-nada_numeric_types = {
-    SecretInteger, audit.abstract.SecretInteger,
-    PublicInteger, audit.abstract.PublicInteger,
-}
-
-
 def nada_lt(
-        item: Union[*nada_numeric_types],
-        cmp: Union[*nada_numeric_types]
-) -> Union[*nada_numeric_types]:
+        item: Union[SecretInteger, audit.abstract.SecretInteger],
+        cmp: Union[SecretInteger, audit.abstract.SecretInteger]
+) -> Union[SecretInteger, audit.abstract.SecretInteger]:
     return (item < cmp).if_else(item, item - item)
 
 
 def nada_lteq(
-        item: Union[*nada_numeric_types],
-        cmp: Union[*nada_numeric_types]
-) -> Union[*nada_numeric_types]:
+        item: Union[SecretInteger, audit.abstract.SecretInteger],
+        cmp: Union[SecretInteger, audit.abstract.SecretInteger]
+) -> Union[SecretInteger, audit.abstract.SecretInteger]:
     return (item <= cmp).if_else(item, item - item)
 
 
 def nada_gt(
-        item: Union[*nada_numeric_types],
-        cmp: Union[*nada_numeric_types]
-) -> Union[*nada_numeric_types]:
+        item: Union[SecretInteger, audit.abstract.SecretInteger],
+        cmp: Union[SecretInteger, audit.abstract.SecretInteger]
+) -> Union[SecretInteger, audit.abstract.SecretInteger]:
     return (item > cmp).if_else(item, item - item)
 
 
 def nada_gteq(
-        item: Union[*nada_numeric_types],
-        cmp: Union[*nada_numeric_types]
-) -> Union[*nada_numeric_types]:
+        item: Union[SecretInteger, audit.abstract.SecretInteger],
+        cmp: Union[SecretInteger, audit.abstract.SecretInteger]
+) -> Union[SecretInteger, audit.abstract.SecretInteger]:
     return (item >= cmp).if_else(item, item - item)
 
 
 def nada_eq(
-        item: Union[*nada_numeric_types],
-        cmp: Union[*nada_numeric_types]
-) -> Union[*nada_numeric_types]:
+        item: Union[SecretInteger, audit.abstract.SecretInteger],
+        cmp: Union[SecretInteger, audit.abstract.SecretInteger]
+) -> Union[SecretInteger, audit.abstract.SecretInteger]:
     return (item == cmp).if_else(item, item - item)
 
 
 def nada_pubeq(
-        item: Union[*nada_numeric_types],
-        cmp: Union[*nada_numeric_types]
-) -> Union[*nada_numeric_types]:
+        item: Union[SecretInteger, audit.abstract.SecretInteger],
+        cmp: Union[SecretInteger, audit.abstract.SecretInteger]
+) -> Union[SecretInteger, audit.abstract.SecretInteger]:
     return (item.public_equals(cmp)).if_else(item, item - item)
 
 
 def nada_filter(
-        argument: Union[List[Union[*nada_numeric_types]], NadaArray],
+        argument: Union[List[Union[SecretInteger, audit.abstract.SecretInteger]], NadaArray],
         op: callable,
-        cmp: Union[*nada_numeric_types]
+        cmp: Union[SecretInteger, audit.abstract.SecretInteger]
 ) -> NadaArray:
-
-    if isinstance(cmp, int):
-        cmp = Integer(cmp)
 
     output = NadaArray()
     for item in argument:
-        if isinstance(item, int):
-            item = Integer(item)
         output.append(op(item, cmp))
     return output
 
 
 def nada_sum(
-        argument: Union[List[Union[*nada_numeric_types]], NadaArray]
-) -> Union[*nada_numeric_types]:
+        argument: Union[List[Union[SecretInteger, audit.abstract.SecretInteger]], NadaArray]
+) -> Union[SecretInteger, audit.abstract.SecretInteger]:
 
     output = argument[0]
     for e in argument[1:]:
-        if type(e) not in nada_numeric_types:
-            raise TypeError(f"all input values must be of the following types: {nada_numeric_types}")
+        if type(e) not in {SecretInteger, audit.abstract.SecretInteger}:
+            raise TypeError(f"all input values must be of type SecretInteger")
         output = output + e
 
     return output
--- src/nada_data/nada_array.py
+++ src/nada_data/nada_array.py
@@ -1,25 +1,18 @@
 from __future__ import annotations
 from typing import List, Union
 from nada_dsl import (
-    SecretInteger, PublicInteger, Integer, audit
+    SecretInteger, audit
 )
-
-
-nada_numeric_types = {
-    SecretInteger, audit.abstract.SecretInteger,
-    PublicInteger, audit.abstract.PublicInteger,
-}
 
 
 class NadaArray(list):
     def __init__(self: NadaArray, *args):
         super().__init__()
 
-        # input argument is list
         if len(args) == 1 and isinstance(args[0], list):
             argument = args[0]
-        # input argument is generator, e.g. NadaArray(i for i in range(5))
         elif args and hasattr(args[0], "__iter__") and not isinstance(args[0], (str, list, tuple)):
+            # input argument is generator, e.g. NadaArray(i for i in range(5))
             argument = list(args[0])
         else:
             argument = args
@@ -29,18 +22,16 @@ class NadaArray(list):
             self.append(item)
 
     @staticmethod
-    def _check_type(item: Union[*nada_numeric_types]):
-        if type(item) not in nada_numeric_types:
-            raise TypeError(
-                f"supplied items must be of the following types: {nada_numeric_types}"
-            )
+    def _check_type(item: Union[SecretInteger, audit.abstract.SecretInteger]):
+        if type(item) not in {SecretInteger, audit.abstract.SecretInteger}:
+            raise TypeError(f"all input values must be of type SecretInteger")
 
-    def append(self, item: Union[*nada_numeric_types]):
+    def append(self, item: Union[SecretInteger, audit.abstract.SecretInteger]):
         self._check_type(item)
 
         super().append(item)
 
-    def extend(self, iterable: Union[NadaArray, List[Union[*nada_numeric_types]]]):
+    def extend(self, iterable: Union[NadaArray, List[Union[SecretInteger, audit.abstract.SecretInteger]]]):
         for item in iterable:
             self.append(item)
 
