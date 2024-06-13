0cbf582759482f2b0652031bd597f5331a56ed36
bengetch <bengetch@gmail.com>
2024-06-13 17:22:08 -0400

    Tests for nada_sum()

--- test/test_basic_ops.py
+++ test/test_basic_ops.py
@@ -4,25 +4,6 @@ from nada_dsl import audit
 from parameterized import parameterized
 
 
-def set_up_parties():
-    """
-    Basic two party setup. Can't do this in unittest built in setUp() function because it
-    needs to run after the abstract.Initialize function.
-    """
-
-    party_one = audit.Party(name="party_one")
-    party_two = audit.Party(name="party_two")
-
-    party_one_input = audit.SecretInteger(audit.Input(name="p1_input", party=party_one))
-    party_two_input = audit.SecretInteger(audit.Input(name="p2_input", party=party_two))
-
-    return party_one, party_one_input, party_two_input
-
-
-def get_output(cmp: audit.SecretInteger, output_party: audit.Party):
-    return audit.Output(cmp, "output", output_party)
-
-
 class TestBasicOps(unittest.TestCase):
 
     def run_test(self, first_val: int, second_val: int, expected: int, cmp_func: callable):
@@ -38,8 +19,9 @@ class TestBasicOps(unittest.TestCase):
         party_one_input = audit.SecretInteger(audit.Input(name="p1_input", party=party_one))
         party_two_input = audit.SecretInteger(audit.Input(name="p2_input", party=party_two))
 
-        output = get_output(
+        output = audit.Output(
             cmp_func(party_one_input, party_two_input),
+            "output",
             party_one
         )
 
